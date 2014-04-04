6e223a686ca4aaf602423b2a91769f8371ee3332
Mattias Bohez <m4tt3rs@gmail.com>
2014-04-04 13:53:36 +0200

    setup grunt

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    banner: '/*!\n' +
+            ' * Bootstrap-walking-extras v<%= pkg.version %>\n' +
+            ' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' */\n',
+
+    clean: {
+      dist: ['dist', 'docs/dist']
+    },
+
+    concat: {
+      opoptions: {
+        report: 'min'
+      },
+      bootstrapWalkingExtras: {
+        src: [
+          'js/modal-responsive.js'
+        ],
+        dest: 'dist/js/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        report: 'min'
+      },
+      bootstrapWalkingExtras: {
+        options: {
+          banner: '<%= banner %>'
+        },
+        src: '<%= concat.bootstrapWalkingExtras.dest %>',
+        dest: 'dist/js/<%= pkg.name %>.min.js'
+      }
+    },
+
+    less: {
+      compileCore: {
+        options: {
+          sourceMap: true,
+          outputSourceFiles: true,
+          sourceMapURL: '<%= pkg.name %>.css.map',
+          sourceMapFilename: 'dist/css/<%= pkg.name %>.css.map'
+        },
+        files: {
+          'dist/css/<%= pkg.name %>.css': 'less/Bootstrap-walking-extras.less'
+        }
+      },
+      minify: {
+        options: {
+          cleancss: true,
+          report: 'min'
+        },
+        files: {
+          'dist/css/<%= pkg.name %>.min.css': 'dist/css/<%= pkg.name %>.css'
+        }
+      }
+    },
+
+    usebanner: {
+      dist: {
+        options: {
+          position: 'top',
+          banner: '<%= banner %>'
+        },
+        files: {
+          src: [
+            'dist/css/<%= pkg.name %>.css',
+            'dist/css/<%= pkg.name %>.min.css'
+          ]
+        }
+      }
+    },
+
+
+  });
+
+ require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+  // JS distribution task.
+  grunt.registerTask('dist-js', ['concat', 'uglify']);
+
+  // CSS distribution task.
+  grunt.registerTask('less-compile', ['less:compileCore']);
+  grunt.registerTask('dist-css', ['less-compile', 'less:minify', 'usebanner']);
+
+  // Full distribution task.
+  grunt.registerTask('dist', ['clean', 'dist-css', 'dist-js']);
+};
